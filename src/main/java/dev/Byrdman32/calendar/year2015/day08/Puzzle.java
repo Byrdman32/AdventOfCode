c5ecbbfd0a003c293f03130518a989d44adcf884
Eli Byrd <edbgkk@mst.edu>
2022-11-02 13:49:59 -0500

    Finished 2015 Day 8 Part 1
    #8

---
+++ src/main/java/dev/Byrdman32/calendar/year2015/day08/Puzzle.java
@@ -0,0 +1,34 @@
+package dev.Byrdman32.calendar.year2015.day08;
+
+import java.util.List;
+import dev.Byrdman32.interfaces.GenericPuzzle;
+
+public class Puzzle implements GenericPuzzle  {
+    public Object solvePart1(List<String> input) {
+
+        int total = 0;
+        int total2 = 0;
+
+        for (String s : input) {
+            total += s.length();
+
+            for (int i = 1; i < s.length() - 1; i++) {
+                if (s.charAt(i) == '\\') {
+                    if (s.charAt(i + 1) == 'x') {
+                        i += 3;
+                    } else {
+                        i++;
+                    }
+                }
+
+                total2++;
+            }
+        }
+        return total - total2;
+    }
+
+    public Object solvePart2(List<String> input) {
+
+        return null;
+    }
+}
