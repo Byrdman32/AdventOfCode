dc35dc51b86bdcf244028e433fc3a3b2bf95b97a
Eli Byrd <edbgkk@mst.edu>
2022-11-03 01:56:21 -0500

    Finished 2015 Day 9 Part 2
    Closes #9

--- src/main/java/dev/Byrdman32/calendar/year2015/day09/Puzzle.java
+++ src/main/java/dev/Byrdman32/calendar/year2015/day09/Puzzle.java
@@ -76,6 +76,51 @@ public class Puzzle implements GenericPuzzle  {
 
     public Object solvePart2(List<String> input) {
 
-        return null;
+        List<String> cities = findCities(input);
+
+        int[][] distances = new int[cities.size()][cities.size()];
+
+        for (String line : input) {
+            String start = line.substring(0, line.indexOf(" "));
+            String end = line.substring(line.indexOf("to ") + 3, line.indexOf(" ="));
+            int distance = Integer.parseInt(line.substring(line.indexOf("= ") + 2));
+
+            int startCity = cities.indexOf(start);
+            int endCity = cities.indexOf(end);
+
+            distances[startCity][endCity] = distance;
+            distances[endCity][startCity] = distance;
+        }
+
+        int longestDistance = 0;
+
+        for (int i = 0; i < cities.size(); i++) {
+            int distance = 0;
+            int currentCity = i;
+            int nextCity = 0;
+            boolean[] visited = new boolean[cities.size()];
+            visited[currentCity] = true;
+
+            for (int j = 0; j < cities.size() - 1; j++) {
+                int maxDistance = 0;
+
+                for (int k = 0; k < cities.size(); k++) {
+                    if (!visited[k] && distances[currentCity][k] != 0 && distances[currentCity][k] > maxDistance) {
+                        maxDistance = distances[currentCity][k];
+                        nextCity = k;
+                    }
+                }
+
+                distance += maxDistance;
+                visited[nextCity] = true;
+                currentCity = nextCity;
+            }
+
+            if (distance > longestDistance) {
+                longestDistance = distance;
+            }
+        }
+
+        return longestDistance;
     }
 }
