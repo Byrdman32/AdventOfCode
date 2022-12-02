ee888d6bc9e8abbf8dc571705bfbef7c977941c8
Eli Byrd <edbgkk@mst.edu>
2022-12-01 21:33:54 -0600

    Finished 2022 Day 1 Part 2
    Closed #176

--- src/main/java/dev/Byrdman32/calendar/year2022/day01/Puzzle.java
+++ src/main/java/dev/Byrdman32/calendar/year2022/day01/Puzzle.java
@@ -1,8 +1,6 @@
 package dev.Byrdman32.calendar.year2022.day01;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import dev.Byrdman32.interfaces.GenericPuzzle;
 
@@ -19,7 +17,6 @@ public class Puzzle implements GenericPuzzle  {
             }
 
             if (input.get(i).isBlank() || i == input.size() - 1) {
-//                System.out.println("Elf " + elf + " has " + temp + " calories.");
                 elfs.put("Elf " + elf, temp);
 
                 elf++;
@@ -40,6 +37,50 @@ public class Puzzle implements GenericPuzzle  {
 
     public Object solvePart2(List<String> input) {
 
-        return null;
+        Map<String, Integer> elfs = new HashMap<String, Integer>();
+        int temp = 0;
+        int elf = 1;
+
+        for (int i = 0; i < input.size(); i++) {
+            if (!input.get(i).isBlank()) {
+                temp += Integer.parseInt(input.get(i));
+            }
+
+            if (input.get(i).isBlank() || i == input.size() - 1) {
+                elfs.put("Elf " + elf, temp);
+
+                elf++;
+                temp = 0;
+            }
+        }
+
+        LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : elfs.entrySet()) {
+            list.add(entry.getValue());
+        }
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        for (int num : list) {
+            for (Map.Entry<String, Integer> entry : elfs.entrySet()) {
+                if (entry.getValue().equals(num)) {
+                    sortedMap.put(entry.getKey(), num);
+                }
+            }
+        }
+
+        int max = 0;
+        int count = 0;
+
+        for (Map.Entry<String, Integer> entry : sortedMap.entrySet()) {
+            if (count < 3) {
+                max += entry.getValue();
+            }
+
+            count++;
+        }
+
+        return max;
     }
 }
