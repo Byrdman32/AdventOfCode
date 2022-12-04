569279d3670e1030f191ee444f5ef2a80d2e931b
Eli Byrd <edbgkk@mst.edu>
2022-12-03 20:49:56 -0600

    Finished 2015 Day 12 Part 2
    #12

--- src/main/java/dev/Byrdman32/calendar/year2015/day12/Puzzle.java
+++ src/main/java/dev/Byrdman32/calendar/year2015/day12/Puzzle.java
@@ -6,6 +6,97 @@ import dev.Byrdman32.interfaces.GenericPuzzle;
 
 public class Puzzle implements GenericPuzzle  {
 
+    public static int countAllNumbers(String document)
+    {
+        int total = 0;
+        StringBuilder buffer = new StringBuilder();
+
+        for (char c : document.toCharArray())
+        {
+            if (Character.isDigit(c) || c == '-')
+            {
+                buffer.append(c);
+                continue;
+            }
+
+            if (buffer.length() > 0)
+            {
+                total += Integer.parseInt(buffer.toString());
+                buffer.setLength(0);
+            }
+        }
+
+        return total;
+    }
+
+    public static String cleanJson(String document)
+    {
+        String illegal = "red";
+
+        while (true)
+        {
+            int objectToRemoveIndex = document.indexOf(illegal);
+            if (objectToRemoveIndex == -1) break;
+
+            String context = document.substring(objectToRemoveIndex - 2, objectToRemoveIndex);
+            if (context.equals(",\"") || context.equals("[\""))
+            {
+                StringBuilder builder = new StringBuilder(document);
+                builder.replace(objectToRemoveIndex, objectToRemoveIndex + illegal.length(), "");
+                document = builder.toString();
+                continue;
+            }
+
+            document = removeContainingObject(document, objectToRemoveIndex);
+        }
+
+        return document;
+    }
+
+    public static String removeContainingObject(String document, int index)
+    {
+        StringBuilder builder = new StringBuilder(document);
+
+        int leftBraceIndex = getLeftEnclosingObjectBrace(document, index);
+        int rightBraceIndex = getRightEnclosingObjectBrace(document, index);
+        builder.replace(leftBraceIndex, rightBraceIndex + 1,"");
+
+        return builder.toString();
+    }
+
+    public static int getLeftEnclosingObjectBrace(String document, int index)
+    {
+        int braceCounter = 0;
+        int i;
+
+        for (i = index; i > 0; i--)
+        {
+            if      (document.charAt(i) == '{' && braceCounter == 0) break;
+            else if (document.charAt(i) == '}') braceCounter++;
+            else if (document.charAt(i) == '{') braceCounter--;
+        }
+
+        if (document.charAt(i) != '{') throw new IllegalArgumentException();
+
+        return i;
+    }
+
+    public static int getRightEnclosingObjectBrace(String document, int index)
+    {
+        int braceCounter = 0;
+        int i;
+
+        for (i = index; i < document.length(); i++)
+        {
+            if      (document.charAt(i) == '}' && braceCounter == 0) break;
+            else if (document.charAt(i) == '{') braceCounter++;
+            else if (document.charAt(i) == '}') braceCounter--;
+        }
+
+        if (document.charAt(i) != '}') throw new IllegalArgumentException();
+
+        return i;
+    }
     public Object solvePart1(List<String> input) {
 
         int count = 0;
@@ -31,6 +122,8 @@ public class Puzzle implements GenericPuzzle  {
 
     public Object solvePart2(List<String> input) {
 
-        return null;
+        String cleanedDocument = cleanJson(input.get(0));
+
+        return countAllNumbers(cleanedDocument);
     }
 }
