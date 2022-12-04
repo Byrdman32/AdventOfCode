04d2620ac02b6b581fdc1ab5648105d1abe2881a
Eli Byrd <edbgkk@mst.edu>
2022-12-04 01:46:26 -0600

    Finished 2015 Day 13 Part 2
    #13

--- src/main/java/dev/Byrdman32/calendar/year2015/day13/Puzzle.java
+++ src/main/java/dev/Byrdman32/calendar/year2015/day13/Puzzle.java
@@ -51,6 +51,45 @@ public class Puzzle implements GenericPuzzle  {
 
     public Object solvePart2(List<String> input) {
 
-        return null;
+        Map<String, Integer> happinessGain = new HashMap<>();
+        Set<String> names = new HashSet<>();
+
+        for(String line : input){
+            Matcher m = HAPPINESS.matcher(line);
+            if(m.find()){
+                happinessGain.put(m.group(1) + ":" + m.group(4), m.group(2).equals("gain") ? Integer.parseInt(m.group(3)) : -Integer.parseInt(m.group(3)));
+                names.add(m.group(1));
+            }else{
+                System.err.println("Couldn't parse line: "+line);
+            }
+        }
+
+        for(String name : names){
+            happinessGain.put(name + ":Me", 0);
+            happinessGain.put("Me:" + name, 0);
+        }
+        names.add("Me");
+
+        Integer maxHappiness = Integer.MIN_VALUE;
+
+        Collection<List<String>> permutations = Collections2.permutations(names);
+        for(List<String> perm : permutations){
+            if(perm.size() != names.size())
+                continue;
+
+            Integer happiness = 0;
+            for(int i = 0; i < perm.size() - 1; i++){
+                happiness += happinessGain.get(perm.get(i) + ":" + perm.get(i + 1));
+                happiness += happinessGain.get(perm.get(i + 1) + ":" + perm.get(i));
+            }
+            happiness += happinessGain.get(perm.get(0) + ":" + perm.get(perm.size() - 1));
+            happiness += happinessGain.get(perm.get(perm.size() - 1) + ":" + perm.get(0));
+
+            if(happiness > maxHappiness){
+                maxHappiness = happiness;
+            }
+        }
+
+        return maxHappiness;
     }
 }
