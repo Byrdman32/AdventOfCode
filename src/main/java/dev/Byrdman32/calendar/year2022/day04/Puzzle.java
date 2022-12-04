9449c28dd8b62e9d55cf28d97b57e26493118823
Eli Byrd <edbgkk@mst.edu>
2022-12-03 23:09:06 -0600

    Finished 2022 Day 4 Part 2
    #179

--- src/main/java/dev/Byrdman32/calendar/year2022/day04/Puzzle.java
+++ src/main/java/dev/Byrdman32/calendar/year2022/day04/Puzzle.java
@@ -30,6 +30,25 @@ public class Puzzle implements GenericPuzzle  {
 
     public Object solvePart2(List<String> input) {
 
-        return null;
+        int totalOverlapping = 0;
+
+        for (String line : input) {
+            String[] split = line.split(",");
+            int[] elf1 = new int[2];
+            int[] elf2 = new int[2];
+
+            elf1[0] = Integer.parseInt(split[0].split("-")[0]);
+            elf1[1] = Integer.parseInt(split[0].split("-")[1]);
+            elf2[0] = Integer.parseInt(split[1].split("-")[0]);
+            elf2[1] = Integer.parseInt(split[1].split("-")[1]);
+
+            if (elf1[0] <= elf2[0] && elf1[1] >= elf2[0]) {
+                totalOverlapping++;
+            } else if (elf2[0] <= elf1[0] && elf2[1] >= elf1[0]) {
+                totalOverlapping++;
+            }
+        }
+
+        return totalOverlapping;
     }
 }
