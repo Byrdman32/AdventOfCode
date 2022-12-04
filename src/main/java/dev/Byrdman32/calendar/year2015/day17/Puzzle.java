2bf145d2793a111ca957470213e4c106f900793f
Eli Byrd <edbgkk@mst.edu>
2022-12-03 22:53:10 -0600

    Finished 2015 Day 17 Part 2
    #17

--- src/main/java/dev/Byrdman32/calendar/year2015/day17/Puzzle.java
+++ src/main/java/dev/Byrdman32/calendar/year2015/day17/Puzzle.java
@@ -21,6 +21,14 @@ public class Puzzle implements GenericPuzzle  {
 
     public Object solvePart2(List<String> input) {
 
-        return null;
+        int[] buckets = input.stream().mapToInt(Integer::parseInt).toArray();
+        int target = 150;
+
+        int[] bitCounts = IntStream.range(1, 1 << buckets.length).filter(a ->
+                IntStream.range(0, buckets.length).map(b -> ((a & (1 << b)) != 0) ? buckets[b] : 0).sum() == target
+        ).map(Integer::bitCount).toArray();
+
+        int minBits = IntStream.of(bitCounts).min().orElse(0);
+        return IntStream.of(bitCounts).filter(i -> i == minBits).count();
     }
 }
