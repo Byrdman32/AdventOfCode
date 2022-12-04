dd062a8a4495e82b3e715877b2757910f1269054
Eli Byrd <edbgkk@mst.edu>
2022-12-04 00:04:51 -0600

    Finished 2015 Day 19 Part 2
    #19

--- src/main/java/dev/Byrdman32/calendar/year2015/day19/Puzzle.java
+++ src/main/java/dev/Byrdman32/calendar/year2015/day19/Puzzle.java
@@ -1,15 +1,28 @@
 package dev.Byrdman32.calendar.year2015.day19;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 import dev.Byrdman32.interfaces.GenericPuzzle;
 
 public class Puzzle implements GenericPuzzle  {
+
+    private static Collection<String> doReplacement(String original, Multimap<String, String> replacements){
+        Set<String> replaced = new HashSet<>();
+        for(String orig : replacements.keySet()){
+            for(String repl : replacements.get(orig)){
+                int index = 0;
+                while((index = original.indexOf(repl, index)) != -1){
+                    replaced.add(original.substring(0, index) + orig + original.substring(index + repl.length()));
+                    index++;
+                }
+            }
+        }
+
+        return replaced;
+    }
+
     public Object solvePart1(List<String> input) {
 
         List<String> lines = new ArrayList<>();
@@ -47,6 +60,61 @@ public class Puzzle implements GenericPuzzle  {
 
     public Object solvePart2(List<String> input) {
 
-        return null;
+        List<String> lines = new ArrayList<>();
+        String molecule = "";
+
+        for (String line : input) {
+            if (line.contains("=>")) {
+                lines.add(line);
+            } else if (!line.isBlank()) {
+                molecule = line;
+            }
+        }
+
+        Multimap<String, String> replacements = ArrayListMultimap.create();
+
+        for(String line : lines){
+            String[] parts = line.split(" => ");
+            replacements.put(parts[0], parts[1]);
+        }
+
+        Integer depth = 0;
+
+        Set<String> currLevel = new HashSet<>();
+        currLevel.add(molecule);
+
+        Set<String> prevSeen = new HashSet<>();
+        prevSeen.add(molecule);
+
+        while(!currLevel.contains("e") && !currLevel.isEmpty()){
+            depth++;
+            Set<String> nextLevel = new HashSet<>();
+            for(String c : currLevel){
+                nextLevel.addAll(doReplacement(c, replacements));
+            }
+
+            nextLevel.removeAll(prevSeen);
+            prevSeen.addAll(nextLevel);
+
+            Set<String> sortedNextLevel = new TreeSet<>(new Comparator<String>() {
+                @Override
+                public int compare(String key1, String key2) {
+                    if(key1.length() >= key2.length()){
+                        return 1;
+                    }else{
+                        return -1;
+                    }
+                }
+            });
+            sortedNextLevel.addAll(nextLevel);
+
+            currLevel = new HashSet<>();
+            Iterator<String> iter = sortedNextLevel.iterator();
+            while(iter.hasNext() && currLevel.size() <= 10){
+                currLevel.add(iter.next());
+            }
+        }
+
+        return depth;
     }
 }
