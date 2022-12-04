f03934bb2f33f67740cc9da7fcaccf1a82df6df3
Eli Byrd <edbgkk@mst.edu>
2022-12-03 23:07:56 -0600

    Finished 2022 Day 4 Part 1
    #179

--- src/main/java/dev/Byrdman32/Calendar.java
+++ src/main/java/dev/Byrdman32/Calendar.java
@@ -15,9 +15,9 @@ import static dev.Byrdman32.util.meta.CommandLineTools.log;
 import static dev.Byrdman32.util.meta.File.read;
 import static java.lang.Thread.currentThread;
 
-@ExecutedDays(day = DAY_17)
+@ExecutedDays(day = DAY_04)
 public class Calendar {
-    private static final String CURRENT_YEAR = "2015";
+    private static final String CURRENT_YEAR = "2022";
 
     @SuppressWarnings({"UnstableApiUsage", "unchecked"})
     @SneakyThrows(value = IOException.class)
