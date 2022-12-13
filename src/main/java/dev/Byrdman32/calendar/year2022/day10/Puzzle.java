a1a296ff2f21303078348fc1fa4602501ea56338
Eli Byrd <edbgkk@mst.edu>
2022-12-13 13:55:39 -0600

    Finished 2022 Day 10 Part 2
    #185

--- src/main/java/dev/Byrdman32/calendar/year2022/day10/Puzzle.java
+++ src/main/java/dev/Byrdman32/calendar/year2022/day10/Puzzle.java
@@ -30,6 +30,34 @@ public class Puzzle implements GenericPuzzle  {
 
     public Object solvePart2(List<String> input) {
 
+        var x = 1;
+        var cycle = 0;
+        char[][] grid = new char[6][40];
+
+        for (var line: input) {
+            var parts = line.split(" ");
+
+            if (parts[0].equals("noop")) {
+                printPixel(grid, cycle, x);
+                cycle++;
+            } else if (parts[0].equals("addx")) {
+                printPixel(grid, cycle, x);
+                cycle++;
+                printPixel(grid, cycle, x);
+                cycle++;
+                x += Integer.parseInt(parts[1]);
+            }
+        }
+
+        System.out.println();
+
+        for (var line: grid) {
+            for (var column: line) {
+                System.out.print(column);
+            }
+            System.out.println();
+        }
+
         return null;
     }
 
@@ -37,5 +65,12 @@ public class Puzzle implements GenericPuzzle  {
         var cycles = List.of(20, 60, 100, 140, 180, 220);
 
         return cycles.contains(cycle) ? cycle * x : 0;
+    }
+
+    private void printPixel(char[][] grid, int cycle, int x) {
+        var row = cycle / 40;
+        var column = cycle % 40;
+
+        grid[row][column] = x >= column - 1 && x <= column + 1 ? '#' : ' ';
     }
 }
